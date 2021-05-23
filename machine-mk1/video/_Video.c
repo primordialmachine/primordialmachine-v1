5652ac619c3906990bf370af4e975e7cd3ccee04
Primordial Machine <contact@primordialmachine.com>
2021-05-23 23:07:50 +0200

    [input] split module
    Split executable module into executable module and "video" module.

---
+++ machine-mk1/video/_Video.c
@@ -0,0 +1,2 @@
+#define MACHINE_VIDEO_PRIVATE (1)
+#include "_Video.h"
---
+++ machine-mk1/video/_Video.h
@@ -0,0 +1,11 @@
+/// @file _Video.h
+/// @author Michael Heilmann <michaelheilmann@primordialmachine.com>
+/// @copyright Copyright (c) 2021 Michael Heilmann. All rights reserved.
+#if !defined(MACHINE__VIDEO_H_INCLUDED)
+#define MACHINE__VIDEO_H_INCLUDED
+
+#define MACHINE_VIDEO_PRIVATE (1)
+#include "Video/CanvasSizeChangedEvent.h"
+#undef MACHINE_VIDEO_PRIVATE
+
+#endif // MACHINE__VIDEO_H_INCLUDED
