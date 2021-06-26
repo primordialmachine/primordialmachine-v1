1157249d8a4470c0ba82a6dc84c79fc5a086141f
Primordial Machine <contact@primordialmachine.com>
2021-06-26 12:45:22 +0200

    [video] improve doc comments

--- machine-mk1/video/Video/Buffer.c
+++ machine-mk1/video/Video/Buffer.c
@@ -1,3 +1,6 @@
+/// @file Video/Buffer.c
+/// @author Michael Heilmann <michaelheilmann@primordialmachine.com>
+/// @copyright Copyright (c) 2021 Michael Heilmann. All rights reserved.
 #include "./../Video/Buffer.h"
 
 
--- machine-mk1/video/Video/Texture.c
+++ machine-mk1/video/Video/Texture.c
@@ -1,3 +1,6 @@
+/// @file Video/Texture.c
+/// @author Michael Heilmann <michaelheilmann@primordialmachine.com>
+/// @copyright Copyright (c) 2021 Michael Heilmann. All rights reserved.
 #include "./../Video/Texture.h"
 
 
