697971135fbcf7e28e3796269286a7bf0ab997e8
Primordial Machine <contact@primordialmachine.com>
2021-06-27 16:41:10 +0200

    [gui] cleanup

---
+++ machine-mk1/machine-mk1/_GUI.c
@@ -0,0 +1,4 @@
+/// @file _GUI.c
+/// @author Michael Heilmann (<michaelheilmann@primordialmachine.com>)
+/// @copyright Copyright (c) 2021 Michael Heilmann. All rights reservied.
+#include "_GUI.h"
---
+++ machine-mk1/machine-mk1/_GUI.h
@@ -0,0 +1,13 @@
+/// @file _GUI.h
+/// @author Michael Heilmann (<michaelheilmann@primordialmachine.com>)
+/// @copyright Copyright (c) 2021 Michael Heilmann. All rights reservied.
+#if !defined(MACHINE__GUI_H_INCLUDED)
+#define MACHINE__GUI_H_INCLUDED
+
+#include "GUI/Border.h"
+#include "GUI/TextButton.h"
+#include "GUI/TextLabel.h"
+
+#include "GUI/WidgetList.h"
+
+#endif // MACHINE__GUI_H_INCLUDED
