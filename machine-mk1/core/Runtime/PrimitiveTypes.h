9e5313e6deaa19ee586646451dfe91656bd1e0a3
Primordial Machine <contact@primordialmachine.com>
2021-07-19 21:50:05 +0200

    [runtime] update Machine_Integer
    Define Machine_Integer as int64_t instead of int32_t.

--- machine-mk1/core/Runtime/PrimitiveTypes.h
+++ machine-mk1/core/Runtime/PrimitiveTypes.h
@@ -67,13 +67,13 @@ INLINE bool Machine_Boolean_isEqualTo(Machine_Boolean x, Machine_Boolean y) {
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 /// @brief The C-level representation of the <code>Integer</code> type.
-typedef int32_t Machine_Integer;
+typedef int64_t Machine_Integer;
 
 /// @brief The greatest <code>Integer</code> value.
-#define Machine_Integer_Greatest INT32_MAX
+#define Machine_Integer_Greatest INT64_MAX
 
 /// @brief The least <code>Integer</code> value.
-#define Machine_Integer_Least INT32_MIN
+#define Machine_Integer_Least INT64_MIN
 
 INLINE size_t Machine_Integer_getHashValue(Machine_Integer x) {
   return (size_t)x;
