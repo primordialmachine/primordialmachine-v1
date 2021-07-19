6f0188f26cda3f4a8328c22780234415cf088b56
Primordial Machine <contact@primordialmachine.com>
2021-07-19 22:46:46 +0200

    [input] add keyboard keys enumeration

---
+++ machine-mk1/input/Input/KeyboardKeys.c
@@ -0,0 +1,22 @@
+/// @file Input/KeyboardKeys.c
+/// @author Michael Heilmann <michaelheilmann@primordialmachine.com>
+/// @copyright Copyright (c) 2021 Michael Heilmann. All rights reserved.
+#define MACHINE_INPUT_PRIVATE (1)
+#include "./../Input/KeyboardKeys.h"
+
+#include <string.h>
+
+Machine_String* Machine_KeyboardKeys_toString(Machine_KeyboardKeys self) {
+#define DEFINE(NAME, STRING) \
+  case Machine_KeyboardKeys_##NAME: { \
+    return Machine_String_create(STRING, strlen(STRING)); \
+  } break;
+
+  switch (self) {
+  #include "./../Input/KeyboardKeys.i"
+  default:
+    MACHINE_ASSERT_UNREACHABLE();
+  };
+
+#undef DEFINE
+}
---
+++ machine-mk1/input/Input/KeyboardKeys.h
@@ -0,0 +1,26 @@
+/// @file Input/KeyboardKeys.h
+/// @author Michael Heilmann <michaelheilmann@primordialmachine.com>
+/// @copyright Copyright (c) 2021 Michael Heilmann. All rights reserved.
+#if !defined(MACHINE_INPUT_KEYBOARDKEYS_H_INCLUDED)
+#define MACHINE_INPUT_KEYBOARDKEYS_H_INCLUDED
+
+#if !defined(MACHINE_INPUT_PRIVATE)
+#error("Do not include this file directly, include `_Input.h` instead.")
+#endif
+
+#include "_Runtime.h"
+
+typedef enum Machine_KeyboardKeys {
+
+#define DEFINE(NAME,STRING) \
+  Machine_KeyboardKeys_##NAME,
+
+#include "./KeyboardKeys.i"
+
+#undef DEFINE
+
+} Machine_KeyboardKeys;
+
+Machine_String* Machine_KeyboardKeys_toString(Machine_KeyboardKeys self);
+
+#endif // MACHINE_INPUT_KEYBOARDKEYS_H_INCLUDED
