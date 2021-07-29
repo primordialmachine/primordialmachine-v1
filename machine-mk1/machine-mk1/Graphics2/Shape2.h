2eac9c8dffe35752fc7e83f4cd311cf613dc9a61
Primordial Machine <contact@primordialmachine.com>
2021-07-29 13:25:11 +0200

    [graphics] improve doc comments

--- machine-mk1/machine-mk1/Graphics2/Shape2.h
+++ machine-mk1/machine-mk1/Graphics2/Shape2.h
@@ -10,7 +10,18 @@
 
 
 
-/// C-level representation of a shape.
+/// C-level representation of a shape in 2D.
+/// 
+/// The canvas is a 2D coordinate system.
+/// The bottom left corner of the canvas is the origin (0,0),
+/// the positive x-axis goes right, the positive y-axis goes up.
+/// A shape covers a not-necessarily convex area in the plane.
+/// 
+/// Each shape has a position which is the bottom left corner of the bounds of the shape.
+/// The bounds are the smallest rectangle enclosing the shape.
+/// Examples:
+/// - rectangle: bounds are the rectangle itself.
+/// - circle: bounds are the smallest rectangle enclosing the circle.
 MACHINE_DECLARE_CLASSTYPE(Machine_Shape2);
 
 struct Machine_Shape2 {
@@ -18,6 +29,9 @@ struct Machine_Shape2 {
   void (*render)(Machine_Shape2* self, float width, float height);
 };
 
+/// @brief Construct this shape with default values.
+/// @param self This shape.
+/// @param numberOfArguments, arguments The arguments.
 void Machine_Shape2_construct(Machine_Shape2* self, size_t numberOfArguments, Machine_Value const* arguments);
 
 void Machine_Shape2_render(Machine_Shape2* self, float width, float height);
