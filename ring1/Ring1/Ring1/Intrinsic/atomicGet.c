235871c8a76ba9b1d2ba027e96b0ccea0f58c9ec
Primordial Machine <contact@primordialmachine.com>
2022-03-25 21:09:28 +0100

    [ring1] add atomic arithmetic functionality
    Add atomic increment, atomic decrement and atomic get functionality.

---
+++ ring1/Ring1/Ring1/Intrinsic/atomicGet.c
@@ -0,0 +1,32 @@
+// Copyright (c) 2019-2022 Michael Heilmann. All rights reserved.
+
+/// @file Ring1/Intrinsic/atomicGet.c
+/// @copyright Copyright (c) 2019-2022 Michael Heilmann. All rights reserved.
+/// @author Michael Heilmann (michaelheilmann@primordialmachine.com)
+
+#include "Ring1/Intrinsic/atomicGet.h"
+
+
+#if defined(_MSC_VER)
+
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#endif // _MSC_VER
+
+
+#if defined(_MSC_VER)
+
+int32_t
+Ring1_Intrinsic_atomicGet_s32
+  (
+    volatile int32_t* variable
+  )
+{
+  MemoryBarrier(); // Issues a full read write barrier for compiler and processor.
+  return *variable;
+}
+
+
+#endif // _MSC_VER
---
+++ ring1/Ring1/Ring1/Intrinsic/atomicGet.h
@@ -0,0 +1,28 @@
+// Copyright (c) 2019-2022 Michael Heilmann. All rights reserved.
+
+/// @file Ring1/Intrinsic/atomicGet.h
+/// @copyright Copyright (c) 2019-2022 Michael Heilmann. All rights reserved.
+/// @author Michael Heilmann (michaelheilmann@primordialmachine.com)
+
+#if !defined(RING1_INTRINSIC_ATOMICGET_H_INCLUDED)
+#define RING1_INTRINSIC_ATOMICGET_H_INCLUDED
+
+
+#if defined(_MSC_VER)
+#include <stdint.h>
+#endif // _MSC_VER
+
+
+#if defined(_MSC_VER)
+
+/// https://documentation.primordialmachine.com/ring1-library/intrinsic-library/Ring1_Intrinsic_atomicGet_s32
+int32_t
+Ring1_Intrinsic_atomicGet_s32
+  (
+    volatile int32_t* variable
+  );
+
+#endif // _MSC_VER
+
+
+#endif // RING1_INTRINSIC_ATOMICGET_H_INCLUDED
