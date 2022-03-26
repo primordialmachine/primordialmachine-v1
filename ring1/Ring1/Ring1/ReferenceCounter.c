79f28ca03da6dbefd66fc44edb06db506eb1725b
Primordial Machine <contact@primordialmachine.com>
2022-03-26 12:59:23 +0100

    [ring1] add reference counter functionality

---
+++ ring1/Ring1/Ring1/ReferenceCounter.c
@@ -0,0 +1,32 @@
+// Copyright (c) 2019-2022 Michael Heilmann. All rights reserved.
+
+/// @file Ring1/ReferenceCounter.c
+/// @copyright Copyright (c) 2019-2022 Michael Heilmann. All rights reserved.
+/// @author Michael Heilmann (michaelheilmann@primordialmachine.com)
+
+#include "Ring1/ReferenceCounter.h"
+
+#include "Ring1/Intrinsic/atomicDecrement.h"
+#include "Ring1/Intrinsic/atomicGet.h"
+#include "Ring1/Intrinsic/atomicIncrement.h"
+
+int32_t
+Ring1_ReferenceCounter_get
+  (
+    Ring1_ReferenceCounter* referenceCounter
+  )
+{ return Ring1_Intrinsic_atomicGet_s32(referenceCounter); }
+
+int32_t
+Ring1_ReferenceCounter_increment
+  (
+    Ring1_ReferenceCounter* referenceCounter
+  )
+{ return Ring1_Intrinsic_atomicIncrement_s32(referenceCounter); }
+
+int32_t
+Ring1_ReferenceCounter_decrement
+  (
+    Ring1_ReferenceCounter* referenceCounter
+  )
+{ return Ring1_Intrinsic_atomicDecrement_s32(referenceCounter); }
---
+++ ring1/Ring1/Ring1/ReferenceCounter.h
@@ -0,0 +1,46 @@
+// Copyright (c) 2019-2022 Michael Heilmann. All rights reserved.
+
+/// @file Ring1/ReferenceCounter.h
+/// @copyright Copyright (c) 2019-2022 Michael Heilmann. All rights reserved.
+/// @author Michael Heilmann (michaelheilmann@primordialmachine.com)
+
+#if !defined(RING1_REFERENCECOUNTER_H_INCLUDED)
+#define RING1_REFERENCECOUNTER_H_INCLUDED
+
+
+#include <inttypes.h>
+
+
+/// @brief The opaque type of a reference counter.
+typedef volatile int32_t Ring1_ReferenceCounter;
+
+
+/// @brief Atomically read a reference counter.
+/// @param referenceCounter A pointer to the reference counter.
+/// @return The value of the reference counter.
+int32_t
+Ring1_ReferenceCounter_get
+  (
+    Ring1_ReferenceCounter* referenceCounter
+  );
+
+/// @brief Atomically increment a reference counter.
+/// @param referenceCounter A pointer to the reference counter.
+/// @return The new value of the reference counter.
+int32_t
+Ring1_ReferenceCounter_increment
+  (
+    Ring1_ReferenceCounter* referenceCounter
+  );
+
+/// @brief Atomically decrement a reference counter.
+/// @param referenceCouner A pointer to the reference counter.
+/// @return The new value of the reference counter.
+int32_t
+Ring1_ReferenceCounter_decrement
+  (
+    Ring1_ReferenceCounter* referenceCounter
+  );
+
+
+#endif // RING1_REFERENCECOUNTER_H_INCLUDED
