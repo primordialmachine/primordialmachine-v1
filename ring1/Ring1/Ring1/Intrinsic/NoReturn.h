1e7f409a48ffb2534e291f7a808c65a343ff5146
Primordial Machine <contact@primordialmachine.com>
2022-05-02 19:46:37 +0200

    [ring1] improve

---
+++ ring1/Ring1/Ring1/Intrinsic/NoReturn.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2019-2022 Michael Heilmann. All rights reserved.
+
+/// @file Ring1/Intrinsic/NoReturn.h
+/// @copyright Copyright (c) 2019-2022 Michael Heilmann. All rights reserved.
+/// @author Michael Heilmann (michaelheilmann@primordialmachine.com)
+
+#if !defined(RING1_INTRINSIC_NORETURN_H_INCLUDED)
+#define RING1_INTRINSIC_NORETURN_H_INCLUDED
+
+#include "Ring1/Intrinsic/Configuration.h"
+
+// https://documentation.primordialmachine.com/api/ring1-library/intrinsic-library/Ring1_NoReturn
+#if defined(__GNUC__)
+  #define Ring1_NoReturn() __attribute__((noreturn))
+#elif defined(_MSC_VER)
+  #define Ring1_NoReturn() __declspec(noreturn)
+#else
+  #error("unsupported compiler")
+#endif
+
+#endif // RING1_INTRINSIC_NORETURN_H_INCLUDED
