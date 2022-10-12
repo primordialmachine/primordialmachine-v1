4686e92ee9bce7131492f5a5601d8be48568692d
Michael Heilmann <contact@primordialmachine.com>
2022-10-12 22:23:15 +0200

    [ring2] update appveyor.yml

--- ring2/Ring2.Library.Test/Ring2/Library/Test/Main.c
+++ ring2/Ring2.Library.Test/Ring2/Library/Test/Main.c
@@ -37,37 +37,10 @@ main
     Ring2_Value result = Ring2_Value_StaticInitializerVoid();
     Ring2_Value arguments[] = { Ring2_Value_StaticInitializerVoid() };
 
-#if 0
-    Ring2_Tests_addTest(tests,
-                        Ring2_String_create(Ring2_Context_get(), "pair-test-1", strlen("pair-test-1")),
-                        &Machine_Collections_Test_PairTest_test1);
-#endif
     Ring2_Test_registerPairTests(tests);
-#if 0
-    Ring2_Tests_addTest(tests,
-                        Ring2_String_create(Ring2_Context_get(), "list-test-1", strlen("list-test-1")),
-                        &Machine_Collections_Test_ListTest_test1);
-#endif
     Ring2_Test_registerArrayListTests(tests);
-#if 0
-    Ring2_Tests_addTest(tests,
-                        Ring2_String_create(Ring2_Context_get(), "map-test-1", strlen("map-test-1")),
-                        &Machine_Collections_Test_MapTest_test1);
-#endif
     Ring2_Test_registerHashMapTests(tests);
-
     Ring2_Test_registerArrayDequeTests(tests);
-#if 0
-    Ring2_Tests_addTest(tests,
-                        Ring2_String_create(Ring2_Context_get(), "array-deque-test-1", strlen("array-deque-test-1")),
-                        &Machine_Collections_Test_ArrayDequeTest_test1);
-    Ring2_Tests_addTest(tests,
-                        Ring2_String_create(Ring2_Context_get(), "array-deque-test-2", strlen("array-deque-test-2")),
-                        &Machine_Collections_Test_ArrayDequeTest_test2);
-    Ring2_Tests_addTest(tests,
-                        Ring2_String_create(Ring2_Context_get(), "array-deque-test-3", strlen("array-deque-test-3")),
-                        &Machine_Collections_Test_ArrayDequeTest_test3);
-#endif
 
     Ring2_Tests_run(tests);
 
