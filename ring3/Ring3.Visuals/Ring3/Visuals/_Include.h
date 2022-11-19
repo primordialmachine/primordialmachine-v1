93b68a36dcaf298223f1dbf18db884f352fc23e8
Michael Heilmann <contact@primordialmachine.com>
2022-11-19 18:48:37 +0100

    [ring3] add visuals

---
+++ ring3/Ring3.Visuals/Ring3/Visuals/_Include.h
@@ -0,0 +1,11 @@
+/// @file Ring3/Visuals/_Include.h
+/// @copyright Copyright (c) 2021-2022 Michael Heilmann. All rights reserved.
+/// @author Michael Heilmann (michaelheilmann@primordialmachine.com)
+
+#if !defined(RING3_VISUALS__INCLUDE_H_INCLUDED)
+#define RING3_VISUALS__INCLUDE_H_INCLUDED
+
+#define RING3_VISUALS_PRIVATE (1)
+#undef RING3_VISUALS_PRIVATE
+
+#endif // RING3_VISUALS__INCLUDE_H_INCLUDED
