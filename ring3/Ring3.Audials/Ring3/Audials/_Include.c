56a86ec6334234fbb85067e27f351472130ffe5e
Michael Heilmann <contact@primordialmachine.com>
2022-11-19 19:00:05 +0100

    [ring3] add audials

---
+++ ring3/Ring3.Audials/Ring3/Audials/_Include.c
@@ -0,0 +1,6 @@
+/// @file Ring3/Audials/_Include.c
+/// @copyright Copyright (c) 2021-2022 Michael Heilmann. All rights reserved.
+/// @author Michael Heilmann (michaelheilmann@primordialmachine.com)
+
+#define RING3_AUDIALS_PRIVATE (1)
+#include "Ring3/Audials/_Include.h"
---
+++ ring3/Ring3.Audials/Ring3/Audials/_Include.h
@@ -0,0 +1,11 @@
+/// @file Ring3/Audials/_Include.h
+/// @copyright Copyright (c) 2021-2022 Michael Heilmann. All rights reserved.
+/// @author Michael Heilmann (michaelheilmann@primordialmachine.com)
+
+#if !defined(RING3_AUDIALS__INCLUDE_H_INCLUDED)
+#define RING3_AUDIALS__INCLUDE_H_INCLUDED
+
+#define RING3_AUDIALS_PRIVATE (1)
+#undef RING3_AUDIALS_PRIVATE
+
+#endif // RING3_AUDIALS__INCLUDE_H_INCLUDED
