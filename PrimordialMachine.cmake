4b245015dd9df11e131f3a66ccab66bad818c51b
Michael Heilmann <contact@primordialmachine.com>
2023-04-13 17:49:37 +0200

    [ring4.demos] update

--- PrimordialMachine.cmake
+++ PrimordialMachine.cmake
@@ -56,7 +56,7 @@ endfunction()
 
 # Define a static library.
 # name The name of the library e.g. "PrimordialMachine.Ring2".
-# Remark: This is the new version of "MyLibrary2" and should soon replace all calls to "MyLibrary".
+# Remark: This is the new version of "MyLibrary" and should soon replace all calls to "MyLibrary".
 function(MyLibrary2 name)
 
   file(GLOB_RECURSE source_files "${CMAKE_CURRENT_SOURCE_DIR}/Sources/*.c")
@@ -73,6 +73,35 @@ function(MyLibrary2 name)
   add_library(${name} STATIC ${files})
   
   target_include_directories(${name} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/Sources)
+  
+  if(MSVC)
+    # Treat certain warnings as errors.
+    target_compile_options(${name} PRIVATE "/we4133")
+    target_compile_options(${name} PRIVATE "/we4090")
+    target_compile_options(${name} PRIVATE "/we4013")
+  endif()
+    
+endfunction()
+
+# Define an executable.
+# name The name of the executable e.g. "PrimordialMachine.Ring2.Test".
+# Remark: This is the new version of "MyExecutable" and should soon replace all calls to "MyExecutable".
+function(MyExecutable2 name)
+
+  file(GLOB_RECURSE source_files "${CMAKE_CURRENT_SOURCE_DIR}/Sources/*.c")
+  source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR}/Sources FILES ${source_files})
+
+  file(GLOB_RECURSE header_files "${CMAKE_CURRENT_SOURCE_DIR}/Sources/*.h")
+  source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR}/Sources FILES ${header_files})
+
+  file(GLOB_RECURSE inlay_files  "${CMAKE_CURRENT_SOURCE_DIR}/*.i")
+  source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR}/Sources FILES ${inlay_files})
+  
+  set(files ${source_files} ${header_files} ${inlay_files})
+  
+  add_executable(${name} ${files})
+  
+  target_include_directories(${name} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/Sources)
   
   if(MSVC)
     # Treat certain warnings as errors.
